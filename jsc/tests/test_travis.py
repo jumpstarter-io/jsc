c8e3f569c7d3b87a3965d9a58e4c6cc3927695bf
Thorwald Lundqvist <thorwald@jumpstarter.io>
2015-03-23 11:09:29 +0100

    tests: explicitly destroy fake endpoint on teardown

--- jsc/tests/test_travis.py
+++ jsc/tests/test_travis.py
@@ -82,6 +82,7 @@ class FakeEndpointConn():
     def stop(self):
         if self.fake_sync_endpoint is not None:
             self.fake_sync_endpoint.terminate()
+            del self.fake_sync_endpoint
             self.fake_sync_endpoint = None
 
     def send(self, data):
