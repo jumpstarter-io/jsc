a501abb1f205e6e27525a6d3e2389c5c7f2c6dac
Thorwald Lundqvist <thorwald@jumpstarter.io>
2015-05-04 13:06:47 +0200

    bump version to v6.3
    [skip ci]

--- jsc/__init__.py
+++ jsc/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "6.2"
+__version__ = "6.3"
 __description__ = "Jumpstarter Console"
 __long_description__ = """
 Jumpstarter Console - A command line tool for managing your Jumpstarter projects.
